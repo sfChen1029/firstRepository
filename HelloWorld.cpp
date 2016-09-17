00a2c0ca61b7fde289cc93e82fa6f06ce5c64a4b
sfChen1029 <steven.fx.chen@gmail.com>
2016-09-17 13:49:39 -0700

    first commit - Steven

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: Sep 9, 2016
+ *      Author: Steven
+ */
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main(){
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
